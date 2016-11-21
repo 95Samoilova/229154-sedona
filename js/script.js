9381537981105349d7d1cc3f48db96b178ab14f3
95Samoilova <95Samoilova@mail.ru>
2016-11-21 22:45:19 +0700

    Исправление ошибок

--- js/script.js
+++ js/script.js
@@ -5,18 +5,16 @@ var date = popup.querySelector("[name=date-hotel-in]");
 var dateout = popup.querySelector("[name=date-hotel-out]");
 
 link.addEventListener("click", function (event) {
-    event.preventDefault();
-    popup.classList.toggle("js-choose-hotel-popup");
-    date.focus();
+  event.preventDefault();
+  popup.classList.toggle("js-choose-hotel-popup");
+  date.focus();
 });
 
 form.addEventListener("submit", function (event) {
-    if (!date.value || !dateout.value) {
-        event.preventDefault();
-        console.log("Нужно ввести даты заезда и выезда");
-    }
+  if (!date.value || !dateout.value) {
+    event.preventDefault();
+  console.log("Нужно ввести даты заезда и выезда");
+  }
 });
-
-var mapOpen = document.querySelector(".js-open-map");
 
 popup.classList.add('js-choose-hotel-popup');
